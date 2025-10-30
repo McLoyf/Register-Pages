ac3d7a098593db6152642635ac33263c36f7e2da
Mcloyf <alexortiz242005@gmail.com>
2025-10-29 19:04:44 -0500

    Add encryption and decryption
    Using AES-256, this runs on the terminal at the moment

--- javascript/register.js
+++ javascript/register.js
@@ -1,5 +1,6 @@
 import fs from "fs";
 import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 const registerUser = async (username, password) => {
     const saltRounds = 10;
@@ -23,3 +24,23 @@ const registerUser = async (username, password) => {
 };
 
 registerUser("alex", "test123");
+
+const algorithm = 'aes-256-cbc';
+const text = 'Hello World';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+let encrypted = cipher.update(text, 'utf8', 'hex');
+encrypted += cipher.final('hex');
+
+const decipher = crypto.createDecipheriv(algorithm, key, iv);
+let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+decrypted += decipher.final('utf8');
+
+
+console.log('Decrypted text:', decrypted);
+console.log('Original Text: ', text);
+console.log('Encrypted Text: ', encrypted);
+console.log('Key (hex): ', key.toString('hex'));
+console.log('IV (hex): ', iv.toString('hex'));

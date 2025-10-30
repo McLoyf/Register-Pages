59e00111063836fd241270f20612a3d9f147ca1b
Mcloyf <alexortiz242005@gmail.com>
2025-10-29 19:42:40 -0500

    Change register.js, now no longer stores paintext in users.json

--- javascript/register.js
+++ javascript/register.js
@@ -2,9 +2,33 @@ import fs from "fs";
 import bcrypt from "bcrypt";
 import crypto from "crypto";
 
+const username = "alex";
+const password = "password123";
+
+// AES-256 Encryption for PII
+const algorithm = 'aes-256-cbc';
+const inputText = 'Hello World';
+
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+
+const hexKey = key.toString('hex');
+const hexIv = iv.toString('hex');
+
+// Encrypt PII
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+let encrypted = cipher.update(username, 'utf8', 'hex');
+encrypted += cipher.final('hex');
+
+// Decrypt PII
+const decipher = crypto.createDecipheriv(algorithm, key, iv);
+let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+decrypted += decipher.final('utf8');
+
+
 const registerUser = async (username, password) => {
     const saltRounds = 10;
-    const hash = await bcrypt.hash(password, saltRounds);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
     let users = [];
     if (fs.existsSync("users.json")) {
@@ -17,30 +41,10 @@ const registerUser = async (username, password) => {
     }
   }
 
-    users.push({ username, password, hash });
+    users.push({ encrypted, hashedPassword, hexKey, hexIv });
 
     fs.writeFileSync("users.json", JSON.stringify(users, null, 2));
     console.log(`User ${username} registered successfully.`);
 };
 
-registerUser("alex", "test123");
-
-const algorithm = 'aes-256-cbc';
-const text = 'Hello World';
-const key = crypto.randomBytes(32);
-const iv = crypto.randomBytes(16);
-
-const cipher = crypto.createCipheriv(algorithm, key, iv);
-let encrypted = cipher.update(text, 'utf8', 'hex');
-encrypted += cipher.final('hex');
-
-const decipher = crypto.createDecipheriv(algorithm, key, iv);
-let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-decrypted += decipher.final('utf8');
-
-
-console.log('Decrypted text:', decrypted);
-console.log('Original Text: ', text);
-console.log('Encrypted Text: ', encrypted);
-console.log('Key (hex): ', key.toString('hex'));
-console.log('IV (hex): ', iv.toString('hex'));
+registerUser(username, "test123");
